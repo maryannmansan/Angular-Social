e6bf753c9cbf9d9b56fd11ccd000b81bd67d4e53
Mary Ann Mansan <nasnamTart@gmail.com>
2018-03-07 14:56:13 +0800

    environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,10 @@
+var config = {
+    apiKey: "AIzaSyBjC1z9dHVwWaYXu_-huReNW5pn8XrdFQg",
+    authDomain: "angular-social-7841d.firebaseapp.com",
+    databaseURL: "https://angular-social-7841d.firebaseio.com",
+    projectId: "angular-social-7841d",
+    storageBucket: "angular-social-7841d.appspot.com",
+    messagingSenderId: "11568024731"
+  };
+  
 
